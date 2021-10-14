ee36725693c3e28205a752a6e3080ec126880955
Delfim Marime <Delfimarime@gmail.com>
2021-10-14 18:26:54 +0200

    wip: update pipeline

--- build/publish.sh
+++ build/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ver=`cat docs/version`
-aws s3 cp ant s3://${{ secrets.AWS_BUCKET }}/repositories/binaries/ant/$VERSION/$S3_DIRECTORY/application
+aws s3 cp ant s3://$SECRET_AWS_BUCKET/repositories/binaries/ant/$VERSION/$S3_DIRECTORY/application
