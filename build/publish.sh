fd9cbce77d545f6fb0c1567efd052c0e942a133e
Delfim Marime <Delfimarime@gmail.com>
2021-10-14 19:29:22 +0200

    wip: windows github action update

--- build/publish.sh
+++ build/publish.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 ver=`cat docs/version`
+
+if [ -z "${S3_FILE}" ]; then
+    filename="ant"
+else
+    filename=$S3_FILE
+fi
+
 aws s3 cp ant s3://$SECRET_AWS_BUCKET/repositories/binaries/ant/$ver/$S3_DIRECTORY/application
